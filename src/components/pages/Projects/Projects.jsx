9ea836c93b75426773e12237b7cc27c03b6c25a2
Mann Kawatkar <mannkawatkar1603@gmail.com>
2024-07-16 17:23:43 +0530

    Projects update

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -1,13 +1,15 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import "./Projects.css";
 import Available from "../../Available/Available";
 import ProjectCard from "../../Project-card/ProjectCard";
 import Footer from "../../Footer/Footer";
 import { Fade } from "react-awesome-reveal";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Projects = () => {
   useEffect(() => {
-    document.title = 'My Projects';
+    document.title = "My Projects";
   }, []);
 
   return (
@@ -21,66 +23,106 @@ const Projects = () => {
             <h1>My Journey</h1>
           </Fade>
 
-          <Fade triggerOnce cascade>
-            <div className="Projects">
-              <ProjectCard
-                name={"CUSINE DELIGHT"}
-                category={"RECIPE WEBSITE IN REACT"}
-                image={"/Projects photos/recipe.png"}
-                CardClass={"large-card"}
-                ImgClass={"large-img"}
-                link={"https://recipes-app-blush.vercel.app/"}
-              />
-              <ProjectCard
-                name={"CARA"}
-                category={"E-COMMERCE WEBSITE "}
-                image={"/Projects photos/e-com.png"}
-                CardClass={"large-card"}
-                ImgClass={"large-img"}
-                link={"https://garments-e-commerce.netlify.app/"}
-              />
-              <ProjectCard
-                name={"WEATHER-APP"}
-                category={"WEATHER-APP IN REACT"}
-                image={"/Projects photos/weather.png"}
-                CardClass={"large-card"}
-                ImgClass={"large-img"}
-                link={"https://weather-app-mann1603.vercel.app/"}
-              />
-              <ProjectCard
-                name={"TESLA"}
-                category={"TESLA CLONE IN REACT"}
-                image={"/Projects photos/tesla.png"}
-                CardClass={"large-card"}
-                ImgClass={"large-img"}
-                link={"https://github.com/mann1603/tesla-clone"}
-              />
-                <ProjectCard
-                  name={"IMDB CLONE"}
-                  category={"IMDB CLONE IN REACT"}
-                  image={"/Projects photos/imdb.png"}
-                  CardClass={"large-card"}
-                  ImgClass={"large-img"}
-                  link={"https://imdb-clone-lyart.vercel.app/"}
-                />
-              <ProjectCard
-                name={"HACKVENGERS"}
-                category={"HACKATHON WEBSITE IN REACT"}
-                image={"/Projects photos/hack.png"}
-                CardClass={"large-card"}
-                ImgClass={"large-img"}
-                link={"https://github.com/mann1603/Hack-vengers"}
-              />
-              <ProjectCard
-                name={"CAFENA"}
-                category={"COFFEE SHOP WEBSITE"}
-                image={"/Projects photos/coffee.png"}
-                CardClass={"large-card"}
-                ImgClass={"large-img"}
-                link={"https://coffee-products.netlify.app/"}
-              />
-            </div>
-          </Fade>
+          <Tabs>
+            <TabList>
+              <Tab> React Projects</Tab>
+              <Tab>WordPress Live Projects</Tab>
+            </TabList>
+
+            <TabPanel>
+              <Fade triggerOnce cascade>
+                <div className="Projects">
+                  <ProjectCard
+                    name={"CUSINE DELIGHT"}
+                    category={"RECIPE WEBSITE IN REACT"}
+                    image={"/Projects photos/recipe.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://recipes-app-blush.vercel.app/"}
+                  />
+                  <ProjectCard
+                    name={"CARA"}
+                    category={"E-COMMERCE WEBSITE "}
+                    image={"/Projects photos/e-com.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://garments-e-commerce.netlify.app/"}
+                  />
+                  <ProjectCard
+                    name={"WEATHER-APP"}
+                    category={"WEATHER-APP IN REACT"}
+                    image={"/Projects photos/weather.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://weather-app-mann1603.vercel.app/"}
+                  />
+                  <ProjectCard
+                    name={"TESLA"}
+                    category={"TESLA CLONE IN REACT"}
+                    image={"/Projects photos/tesla.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://github.com/mann1603/tesla-clone"}
+                  />
+                  <ProjectCard
+                    name={"IMDB CLONE"}
+                    category={"IMDB CLONE IN REACT"}
+                    image={"/Projects photos/imdb.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://imdb-clone-lyart.vercel.app/"}
+                  />
+                  <ProjectCard
+                    name={"HACKVENGERS"}
+                    category={"HACKATHON WEBSITE IN REACT"}
+                    image={"/Projects photos/hack.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://github.com/mann1603/Hack-vengers"}
+                  />
+                  <ProjectCard
+                    name={"CAFENA"}
+                    category={"COFFEE SHOP WEBSITE"}
+                    image={"/Projects photos/coffee.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://coffee-products.netlify.app/"}
+                  />
+                </div>
+              </Fade>
+            </TabPanel>
+
+            <TabPanel>
+            <Fade triggerOnce cascade>
+                <div className="Projects">
+                  <ProjectCard
+                    name={"STUDIO JENNIFER"}
+                    category={"STUDIO JENNIFER"}
+                    image={"/Projects photos/studiojennifer.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://studiojennifer.in/"}
+                  />
+                  <ProjectCard
+                    name={"SHAKTI SPORTS"}
+                    category={"SHAKTI SPORTS CLUB"}
+                    image={"/Projects photos/shakti.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://shaktisportsclub.com/"}
+                  />
+                  <ProjectCard
+                    name={"MEHTAB RESTAURANT"}
+                    category={"MEHTAB RESTAURANT"}
+                    image={"/Projects photos/mehtab.png"}
+                    CardClass={"large-card"}
+                    ImgClass={"large-img"}
+                    link={"https://www.mehtabrestaurant.com/"}
+                  />
+                </div>
+              </Fade>
+            </TabPanel>
+          </Tabs>
         </div>
       </div>
 
